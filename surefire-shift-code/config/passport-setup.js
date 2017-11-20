76b4641db1e3d2827519698f9ad887e1bdc2d4af
Alex Del Valle <amd407@gmail.com>
2017-11-20 15:20:26 -0500

    Setup files and add user model

---
+++ surefire-shift-code/config/passport-setup.js
@@ -0,0 +1,135 @@
+const passport = require("passport");
+const FbStrategy = require("passport-facebook").Strategy;
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+const UserModel = require("../models/user-model");
+
+
+// serialize (what user information do we save to the session?)
+// ------------------------------------------------------------
+// gets called when a user logs in
+passport.serializeUser( (userFromDb, callback) => {
+    //      null is for saying "no errors occurred"
+    //        |
+    callback(null, userFromDb._id);
+            //                 |
+            // save only the "_id" of the user
+});
+
+// deserialize (how do we retrieve the user details from the session?)
+// -------------------------------------------------------------------
+// gets called EVERY TIME you visit a page on the site while you are logged in
+// (this is so we can personalize all pages for the user)
+passport.deserializeUser( (idFromSession, callback) => {
+    UserModel.findById(idFromSession)
+        .then( (userFromDb) => {
+            callback(null, userFromDb);
+            // send Passport the logged in user's info
+            // null is saying "no errors occurred" during the deserialize process
+        })
+        .catch( (err) => {
+            callback(err);
+        });
+});
+
+// STRATEGIES (npm packages that enable additional methods of logging in)
+
+// Login with facebook
+
+passport.use(
+  new FbStrategy(
+    //1st argument of FbStrategy --> settings object
+    {
+        // Facebook credentials
+        clientID: "?????", // App ID
+        clientSecret: "?????", // App Secret
+        // where to go after the log in is successful (one of our routes)
+        callbackURL: "/facebook/success"
+    },
+
+    //2nd argument of FbStrategy --> callback
+    (accessToken, refreshToken, profile, callback) => {
+        // profile contains the user info we get from facebook
+        console.log('FACEBOOK profile ----------------------');
+        console.log(profile);
+
+        // Check if there's already a document in the database for this user
+        UserModel.findOne({ facebookID: profile.id})
+        .then( (userFromDb) => {
+            // if there's already a user account...
+            if (userFromDb) {
+                // tell Passport to use that user account
+                callback(null, userFromDb);
+                return;
+            }
+            // create a user account if there is none
+            const theUser = new UserModel({
+                facebookID: profile.id,
+                fullName: profile.displayName,
+
+            });
+
+            return theUser.save();
+        })
+        .then( (newUser) => {
+            // tell Passport to use the new user account
+            callback(null, newUser);
+        })
+        .catch( (err) => {
+            // tell Passport there was an error in the login process
+            callback(err);
+        });
+    }
+  ) // new FbStrategy()
+); // passport.use()
+
+// Log in with Google
+
+passport.use(
+  new GoogleStrategy(
+    // 1st argument of GoogleStrategy -> settings object
+    {
+      clientID: "????",
+      clientSecret: "????",
+
+      // where to go after log in is successful (one of our routes)
+      callbackURL: "/google/success",
+
+      // fixes Google log in for production
+      proxy: true
+    },
+
+    // 2nd argument of GoogleStrategy -> callback
+    (accessToken, refreshToken, profile, callback) => {
+        console.log('GOOGLE profile --------------------------');
+        console.log(profile);
+
+        // check to see if there's already a document in the database for this user
+        UserModel.findOne({ googleID: profile.id })
+        .then( (userFromDb) => {
+            // if there's already a user account...
+            if (userFromDb) {
+                // tell Passport to use that user account
+                callback(null, userFromDb);
+                return;
+            }
+
+            // create a user account if there is none
+            const theUser = new UserModel({
+                googleID: profile.id,
+                // use the email as their name because Google doesn't give name
+                fullName: profile.displayName
+            });
+
+            return theUser.save();
+        })
+        .then( (newUser) => {
+            // tell Passport to use the new user account
+            callback(null, newUser);
+        })
+        .catch( (err) => {
+            callback(err);
+        });
+    }
+  ) // new GoogleStrategy
+); // passport.use()
---
+++ surefire-shift-code/models/user-model.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+      // User's full name
+      fullName: {
+          type: String,
+          required: [true, "Please provide your name."]
+      },
+
+      // User's primary email address
+      email: {
+          type: String,
+          match: [/.+@.+/, "Email needs an @ sign."],
+          required: [true, "Please provide your email address."]
+      },
+
+      // User's Facebook credentials
+      facebookID: { type: String },
+
+      // User's Google credentials
+      googleID: { type: String },
+
+      // User's phone number (optional)
+      phoneNumber: {
+          type: Number
+      },
+
+      // User's encrypted password
+      encryptedPassword: {
+          type: String
+      },
+
+      // User's Badge number
+      badgeNumber: {
+          type: Number,
+          required: [true, "Please provide your badge number."]
+      },
+
+      // Image of User
+      photoUrl: {
+        type: String
+      },
+
+      // Rank of user
+      rank: {
+          type: String,
+          required: [true, "Please provide your rank."]
+      },
+
+      // User's shift (A, B or C)
+      shift: {
+          type: String,
+          required: [true, "Please provide your shift."]
+      },
+
+      // User's rest day (day of the week)
+      restDay: {
+          type: String,
+          required: [true, "Please provide your rest day"]
+      },
+
+      // Groups user has joined
+      groups: [],
+      station: {
+        type: Number,
+      },
+
+      // User's direct messages
+      messages: [],
+  },
+
+  // Date user was created/updated
+  {
+      timestamps: true
+  }
+);
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
